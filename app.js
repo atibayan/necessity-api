4257796a1c325a458d6fdc348ced0fcb4f6919ed
atibayan <aemilia.tibayan@gmail.com>
2023-02-15 23:15:49 -0800

    added auth token

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
+app.use(cors({
+  origin: `http://localhost:3000`
+}))
+
 const User = require ("./model/User")
+
+
+const checkJwt = (req, res, next) => {
+  console.log(`I am in checkJwt middleware`)
+  const token = req.headers.authorization.split(' ')[1];
+  console.log(token)
+  next()
+}
 
 // Middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
+app.get('/', checkJwt, (req, res) => {
   res.send(`You've reached the app.`)
+})
+
+app.get("/cart", checkJwt, (req, res) => {
+  res.send(`JWT token has been validated.`)
 })
 
 app.post('/register', async (req, res) => {
--- model/User.js
+++ model/User.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user'
   }
 })
 
